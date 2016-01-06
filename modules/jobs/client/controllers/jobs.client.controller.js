e54036533ec1654c7a23951b8940df3a5bdcf48b
Ben Wong <bwong199@my.bcit.ca>
2016-01-06 10:18:49 -0700

    First Commit

---
+++ modules/jobs/client/controllers/jobs.client.controller.js
@@ -0,0 +1,98 @@
+'use strict';
+
+// jobs controller
+angular.module('jobs').controller('JobsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Jobs',
+  function ($scope, $stateParams, $location, Authentication, Jobs) {
+    $scope.authentication = Authentication;
+
+    // Create new job
+    $scope.create = function (isValid) {
+      $scope.error = null;
+
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'jobForm');
+
+        return false;
+      }
+
+      // Create new job object
+      var job = new Jobs({
+        title: this.title,
+        company: this.company, 
+        description: this.description, 
+        hourly_wage: this.hourly_wage, 
+        requirements: this.requirements, 
+        state: this.state, 
+        contact_email: this.contact_email, 
+      });
+
+      console.log(job);
+
+      // Redirect after save
+      job.$save(function (response) {
+        $location.path('jobs/' + response._id);
+
+        // Clear form fields
+        $scope.title = '';
+        $scope.company = '';
+        $scope.description = '';
+        $scope.hourly_wage = '';
+        $scope.requirements = '';
+        $scope.state = '';
+        $scope.contact_email = '';
+      }, function (errorResponse) {
+        $scope.error = errorResponse.data.message;
+      });
+    };
+
+    // Remove existing job
+    $scope.remove = function (job) {
+      if (job) {
+        job.$remove();
+
+        for (var i in $scope.jobs) {
+          if ($scope.jobs[i] === job) {
+            $scope.jobs.splice(i, 1);
+          }
+        }
+      } else {
+        $scope.job.$remove(function () {
+          $location.path('jobs');
+        });
+      }
+    };
+
+    // Update existing Job
+    $scope.update = function (isValid) {
+      $scope.error = null;
+
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'jobForm');
+
+        return false;
+      }
+
+      var job = $scope.job;
+
+      console.log(job);
+
+      job.$update(function () {
+        $location.path('jobs/' + job._id);
+      }, function (errorResponse) {
+        $scope.error = errorResponse.data.message;
+      });
+    };
+
+    // Find a list of jobs
+    $scope.find = function () {
+      $scope.jobs = Jobs.query();
+    };
+
+    // Find existing job
+    $scope.findOne = function () {
+      $scope.job = Jobs.get({
+        jobId: $stateParams.jobId
+      });
+    };
+  }
+]);
